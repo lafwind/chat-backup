fd3e0bbc240dc88b93b88c16fddfa210132e3cb6
Lafwind <lafwind@gmail.com>
2016-07-11 22:49:46 +0800

    Redis !!!

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   config.action_cable.url = "wss://lafwind-chat.daoapp.io/cable"
-  config.action_cable.allowed_request_origins = ['http://http://lafwind-chat.daoapp.io']
+  config.action_cable.allowed_request_origins = ['http://lafwind-chat.daoapp.io']
 
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
