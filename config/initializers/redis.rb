a41cdf8efa5d89c170739465a3fd1d08162a96f5
Lafwind <lafwind@gmail.com>
2016-07-11 23:45:52 +0800

    redis !!!!!

---
+++ config/initializers/redis.rb
@@ -0,0 +1,3 @@
+# Redis::Objects.redis = Redis.new(:host => ( ENV["REDIS_PORT_6379_TCP_ADDR"] || '127.0.0.1'),
+#                                  :port => ( ENV["REDIS_PORT_6379_TCP_PORT"] || '6379').to_i,
+#                                  :password => (ENV["REDIS_PASSWORD"] || ''))
